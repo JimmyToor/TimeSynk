8ab6afa49f0243a9a5b96ed027314a001c3473e3
Jimmy <JimmySToor@protonmail.com>
2025-01-23 14:17:56 -0800

    - Fix styling on games to better hide buttons

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -8,24 +8,26 @@ module GamesHelper
   # @param only_art [Boolean] If true, only the game artwork will be displayed. Default is false.
   # @param only_name [Boolean] If true, only the game name will be displayed. Default is false.
   # @param img_size [String] The size of the game artwork to display. Default is GameImageSize::THUMB.
-  # @param div_classes [String] Additional CSS classes for the container div. Default is an empty string.
+  # @param fig_classes [String] Additional CSS classes for the container div. Default is an empty string.
   # @param img_classes [String] Additional CSS classes for the game art or name. Default is an empty string.
   # @param image_options [GameImageSize] Additional options to pass to the `image_tag` helper for the game artwork.
   # @return [String] The HTML-safe string generated for the game display.
-  def display_game_art_and_name(game:, only_art: false, only_name: false, img_size: GameImageSize::THUMB, div_classes: "", img_classes: "", **image_options)
-    content_tag(:div, class: "game-display #{div_classes}") do
+  def display_game_art_and_name(game:, only_art: false, only_name: false, img_size: GameImageSize::THUMB, fig_classes: "", img_classes: "", **image_options)
+    content_tag(:figure, class: "game-display #{fig_classes}") do
       unless only_name
-        concat image_tag(game.cover_image_url.sub("thumb", img_size),
-          alt: game.name,
-          class: "game-art #{img_classes}",
-          id: dom_id(game, :art),
-          data: {proposal_selection_target: "gameArt"},
-          **image_options) if game.cover_image_url.present?
+        if game.cover_image_url.present?
+          concat image_tag(game.cover_image_url.sub("thumb", img_size),
+            alt: game.name,
+            class: "game-art #{img_classes}",
+            id: dom_id(game, :art),
+            data: {proposal_selection_target: "gameArt"},
+            **image_options)
+        end
       end
       unless only_art
         concat content_tag(:figcaption,
           game.name,
-          class: "game-name #{img_classes}",
+          class: "game-name text-wrap",
           data: {proposal_selection_target:"gameName"},
           id: dom_id(game, :name))
       end
