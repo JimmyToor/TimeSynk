0e9989cdbcc5931e60a676e4e92078ce8c5d9d22
Jimmy <JimmySToor@protonmail.com>
2025-02-19 13:01:50 -0800

    - Change start_date and end_date for Schedule columns to start_time and end_time to be more consistent with Schedule object naming

--- db/migrate/20240606214735_create_schedules.rb
+++ db/migrate/20240606214735_create_schedules.rb
@@ -2,8 +2,8 @@ class CreateSchedules < ActiveRecord::Migration[7.1]
   def change
     create_table :schedules do |t|
       t.references :user, null: false, foreign_key: true
-      t.datetime :start_date, index: {unique: false}
-      t.datetime :end_date, index: {unique: false}
+      t.datetime :start_time, index: {unique: false}
+      t.datetime :end_time, index: {unique: false}
       t.interval :duration
       t.jsonb :schedule_pattern, null: false, default: {}
       t.text :name, null: false, default: "New Schedule"
