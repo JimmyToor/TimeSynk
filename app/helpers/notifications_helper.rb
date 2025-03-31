3607cfa71a010ff4f38714ff81a08040ad3e40dd
Jimmy <JimmySToor@protonmail.com>
2025-03-31 13:40:27 -0700

    - Improve permission set table visibility for smaller displays - Add notification partial

---
+++ app/helpers/notifications_helper.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+module NotificationsHelper
+  # Displays a notification message with a dismiss button.
+  # @param [Hash] options Options for customizing the notification. Valid keys are: :highlight, :list_items, :link
+  #
+  #  - :highlight [String] The text in the message to highlight. Only highlights the first occurrence.
+  #  - :list_items [Array] An array of items to display as a list below the message.
+  #  - :link [Hash] A hash containing the link text and URL. Example: { text: "Click here", url: "/path" }. Only links the first occurrence.
+  def format_notification_message(message, options = {})
+    Rails.logger.debug "Initial notification message: #{message}, options: #{options}"
+    return message unless options.present?
+    options = options.with_indifferent_access if options.respond_to?(:with_indifferent_access)
+
+    if options[:list_items].present?
+      Rails.logger.debug "Notification message before list items: #{message}"
+      message = safe_join([
+        message,
+        content_tag(:div) do
+          content_tag(:ul, class: "mt-1.5 list-disc list-inside") do
+            options[:list_items].map do |item|
+              content_tag(:li, item)
+            end
+          end
+        end
+      ])
+      Rails.logger.debug "Notification message after list items: #{message}"
+    end
+
+    if options[:highlight]
+      Rails.logger.debug "Notification message before highlight: #{message}"
+      parts = message.split(options[:highlight])
+      message = safe_join([
+        parts.first,
+        content_tag(:strong, options[:highlight], class: "font-bold text-primary-600"),
+        parts.last
+      ])
+      Rails.logger.debug "Notification message after highlight: #{message}"
+    end
+
+    if options[:link]
+      Rails.logger.debug "Notification message before link: #{message}"
+      parts = message.split(options[:link][:text])
+      message = safe_join([
+        parts.first,
+        link_to(options[:link][:text], options[:link][:url], class: "text-gray-900 dark:text-white underline
+           decoration-2 hover:decoration-4 decoration-secondary-500 dark:decoration-secondary-200"),
+        parts.last
+      ])
+      Rails.logger.debug "Notification message after link: #{message}"
+    end
+    Rails.logger.debug "Final notification message: #{message}"
+    message
+  end
+
+  def notification_classes(type)
+    case type.to_sym
+    when :notice
+      "text-blue-800 border-t-4 border-blue-300 bg-blue-50 dark:text-blue-400 dark:bg-gray-800 dark:border-blue-800"
+    when :alert, :error
+      "text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
+    when :success
+      "text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800"
+    when :danger, :warning
+      "text-yellow-800 border-t-4 border-yellow-300 bg-yellow-50 dark:text-yellow-300 dark:bg-gray-800 dark:border-yellow-800"
+    when :info
+      "border-gray-300 bg-gray-50 dark:bg-gray-800 dark:border-gray-600"
+    else
+      ""
+    end
+  end
+
+  def dismiss_button_classes(type)
+    case type.to_sym
+    when :notice
+      "text-blue-500 hover:bg-blue-100 focus:ring-blue-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    when :alert, :error
+      "text-red-500 hover:bg-red-100 focus:ring-red-800 dark:hover:bg-red-700 dark:focus:ring-red-800"
+    when :success
+      "text-green-500 hover:bg-green-100 focus:ring-green-800 dark:hover:bg-green-700 dark:focus:ring-green-800"
+    when :danger, :warning
+      "text-yellow-500 hover:bg-yellow-100 focus:ring-yellow-800 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
+    when :info
+      "text-gray-500 hover:bg-gray-100 focus:ring-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+    else
+      ""
+    end
+  end
+end
