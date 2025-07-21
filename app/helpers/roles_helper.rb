421d7acc424d0ec8765d423f209f11704313a12b
Jimmy <JimmySToor@protonmail.com>
2025-07-21 14:02:37 -0700

    - Change more strings to translation keys

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 module RolesHelper
+  def format_group_role(role)
+    I18n.t("group.role.#{role.name}", default: role.name.humanize)
+  end
 
+  def format_game_proposal_role(role)
+    I18n.t("game_proposal.role.#{role.name}", default: role.name.humanize)
+  end
+
+  def format_game_session_role(role)
+    I18n.t("game_session.role.#{role.name}", default: role.name.humanize)
+  end
 end
