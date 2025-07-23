19ab5ac1c7a56c9f18de93ed3dd4233a4962bb43
Jimmy <JimmySToor@protonmail.com>
2025-07-23 16:17:43 -0700

    - Add user identifier to connection.rb

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if (session_record = Session.find_by_id(cookies.signed[:session_token]))
+        session_record.user
+      else
+        reject_unauthorized_connection
+      end
+    rescue ActiveRecord::RecordNotFound
+      reject_unauthorized_connection
+    end
   end
 end
