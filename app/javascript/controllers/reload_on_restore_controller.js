62253f8f4fa54d249312d5c52ce8c78a9d23fdf3
Jimmy <JimmySToor@protonmail.com>
2025-07-25 15:22:52 -0700

    - Prevent reload_on_restore_controller.js from restoring on initial visits

--- app/javascript/controllers/reload_on_restore_controller.js
+++ app/javascript/controllers/reload_on_restore_controller.js
@@ -3,18 +3,21 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="reload-on-restore"
 // Reloads the turbo frame on a restoration visit to prevent stale content
 export default class extends Controller {
-  static values = { src: { type: String, default: "/" } };
-  connect() {
-    if (!document.documentElement.hasAttribute("data-turbo-visit-direction"))
-      return;
+  static values = { src: { type: String } };
 
-    const direction = document.documentElement.getAttribute(
-      "data-turbo-visit-direction",
-    );
-    if (direction === "back" || direction === "forward") {
-      this.element.src = this.srcValue
-        ? this.srcValue
-        : this.element.dataset.src;
-    }
+  connect() {
+    document.addEventListener("turbo:load", this.reloadOnRestore);
   }
+
+  disconnect() {
+    document.removeEventListener("turbo:load", this.reloadOnRestore);
+  }
+
+  reloadOnRestore = (event) => {
+    // A visitStart without a requestStart indicates a restoration visit
+    if (!event.detail.timing?.requestStart && event.detail.timing?.visitStart) {
+      this.element.src =
+        this.srcValue || this.element.dataset.src || this.element.src;
+    }
+  };
 }
