c6110057f659baa52b88806cbf89fb0f9e49f713
Jimmy <Jimmy.Toor.S@Gmail.com>
2025-08-28 14:39:28 -0700

    - Remove redundant configuration from sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,17 +2,9 @@ require "sidekiq"
 require "sidekiq-cron"
 
 if defined?(Sidekiq) && Rails.const_defined?(:Server)
-  Sidekiq.configure_server do |config|
-    config.redis = {url: ENV.fetch("REDIS_URL")}
-
-    schedule_file = "config/sidekiq.yml"
-    if File.exist?(schedule_file)
-      schedule = YAML.load_file(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash(schedule["schedule"] || schedule[:schedule])
-    end
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {url: ENV.fetch("REDIS_URL")}
+  schedule_file = "config/sidekiq.yml"
+  if File.exist?(schedule_file)
+    schedule = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(schedule["schedule"] || schedule[:schedule])
   end
 end
